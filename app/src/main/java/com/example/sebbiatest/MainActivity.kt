9137c7001f7625c645649ec1b1dbc541e7a2bf2a
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-11-05 13:46:18 +0300

    added required dependencies
    - added package structure and base url
    - added blank NewsClient

--- app/src/main/java/com/example/sebbiatest/MainActivity.kt
+++ app/src/main/java/com/example/sebbiatest/MainActivity.kt
@@ -1,5 +1,4 @@
-Npackage com.example.sebbiatest
-
+import com.example.sebbiatest.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
--- app/src/main/java/com/example/sebbiatest/core/util/Constants.kt
+++ app/src/main/java/com/example/sebbiatest/core/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.example.sebbiatest.core.util
 
 object Constants {
-    val BASE_URL = "https://testtask.sebbia.com/"Resource
+    val BASE_URL = "https://testtask.sebbia.com/"
 }
