e1cff30a389dcf10c026dd3ce82f26f83569e8a6
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2023-11-05 13:29:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sebbiatest/core/NewsClient.kt
@@ -0,0 +1,4 @@
+package com.example.sebbiatest.core
+
+object NewsClient {
+}
---
+++ app/src/main/java/com/example/sebbiatest/core/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.sebbiatest.core.util
+
+object Constants {
+    val BASE_URL = "https://testtask.sebbia.com/"Resource
+}
